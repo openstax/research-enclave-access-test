76c0ee1e4043460e4b8d71d11b8c57c4910f90d5
Ross Reedstrom <reedstrm@gmail.com>
2021-12-15 10:17:50 -0600

    pylint; try mount then local path for data read

--- Highlights_BarGraph.py
+++ Highlights_BarGraph.py
@@ -8,22 +8,25 @@ Created on Sat Nov 20 13:21:09 2021
 
 import pandas as pd
 import matplotlib.pyplot as plt
-smpl_df= pd.read_csv("//Users//yomarcummings//Downloads//highlights_data_sample.csv")
+try:
+    smpl_df = pd.read_csv("/data/highlights_data_sample.csv")
+except FileNotFoundError:
+    smpl_df = pd.read_csv("./data/highlights_data_sample.csv")
 
-top_10=smpl_df.head(10)
 
-#top_10.set_index("occurred_at", inplace=True)
-#Above line makes the "occurred_at" column as the index
+top_10 = smpl_df.head(10)
 
-xAxis=["Yellow", "Green", "Pink","Blue"]
-yAxis=[6,2,1,1]
-#Results obtained from top_10["color"].value_counts()
+# top_10.set_index("occurred_at", inplace=True)
+# Above line makes the "occurred_at" column as the index
 
-plt.figure(figsize=(9,3))
-plt.bar(xAxis,yAxis,color=["yellow", "green", "pink","blue"])
+xAxis = ["Yellow", "Green", "Pink", "Blue"]
+yAxis = [6, 2, 1, 1]
+# Results obtained from top_10["color"].value_counts()
+
+plt.figure(figsize=(9, 3))
+plt.bar(xAxis, yAxis, color=["yellow", "green", "pink", "blue"])
 plt.title("Highlight Frequency vs Color")
 plt.xlabel("Color")
 plt.ylabel("Frequency")
-#plt.grid(True)
+# plt.grid(True)
 plt.show()
-
